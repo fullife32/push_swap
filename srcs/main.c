40bfdc2822a6a85ee74f75190e7f90ed1fa9a9a5
Eithan ASSOULINE <eassouli@e2r5p14.42.fr>
2021-06-16 17:37:31 +0200

    some modifications

--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,20 @@
-// #include "libft.h"
+#include "libft.h"
 #include <stdio.h>
 
 int	main(int ac, char **av)
 {
 	(void)av;
+	int *a_stack;
+	int *b_stack;
+
 	if (ac < 2)
 		return (0);
-	else
-		printf("test");
+	a_stack = malloc(sizeof(int) * ac - 1);
+	if (a_stack == NULL)
+		return (-1);
+	b_stack = malloc(sizeof(int) * ac - 1);
+	if (b_stack == NULL)
+		return (-1);
+	printf("%dtest\n", a_stack[0]);
 	return (0);
 }
